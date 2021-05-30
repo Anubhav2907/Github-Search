de809e26c86b732e4a7c1977430714b45995737b
Anubhav2907 <u19ec135@eced.svnit.ac.in>
2021-05-30 20:25:45 +0530

    2 charts done

--- client/src/components/Repos.js
+++ client/src/components/Repos.js
@@ -1,9 +1,66 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React from "react";
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = React.useContext(GithubContext);
+  let languages = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item;
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1, stars: stargazers_count };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
+      };
+    }
+    return total;
+  }, {});
+  const mostUsed = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+  console.log(languages);
+  const mostPopular = Object.values(languages)
+    .sort((a, b) => {
+      return b.stars - a.stars;
+    })
+    .map((item) => {
+      return { ...item, value: item.stars };
+    })
+    .slice(0, 5);
+  console.log(languages);
+  const chartData = [
+    {
+      label: "HTML",
+      value: "10",
+    },
+    {
+      label: "CSS",
+      value: "15",
+    },
+    {
+      label: "JS",
+      value: "60",
+    },
+    {
+      label: "EJS",
+      value: "15",
+    },
+  ];
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <Pie3D data={mostUsed}></Pie3D>
+        <Column3D data={chartData}></Column3D>
+        <Doughnut2D data={mostPopular}></Doughnut2D>
+        <Bar3D data={chartData}></Bar3D>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { Info, Repos, User, Search, Navbar } from '../components';
-import loadingImage from '../images/preloader.gif';
-import { GithubContext } from '../context/context';
+import React from "react";
+import { Info, Repos, User, Search, Navbar } from "../components";
+import loadingImage from "../images/preloader.gif";
+import { GithubContext } from "../context/context";
 const Dashboard = () => {
   return (
     <main>
       <Info></Info>
       <User></User>
+      <Repos></Repos>
     </main>
   );
 };
