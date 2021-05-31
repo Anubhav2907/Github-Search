536172b3cbdcbd18e5a18a75a7287b5e5674fb61
Anubhav2907 <u19ec135@eced.svnit.ac.in>
2021-05-31 14:15:31 +0530

    auth done

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,19 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
-
+//dev-k6k96qtv.us.auth0.com
+//34ZP26CdAE0G6F2WlgW0YwFLdEpEKfMd
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-k6k96qtv.us.auth0.com"
+      clientId="34ZP26CdAE0G6F2WlgW0YwFLdEpEKfMd"
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
