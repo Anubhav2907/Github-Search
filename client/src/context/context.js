9bdfa03931e22554b739f73cfc119e037e664fdb
Anubhav2907 <u19ec135@eced.svnit.ac.in>
2021-05-31 11:58:45 +0530

    completed, auth left

--- client/src/context/context.js
+++ client/src/context/context.js
@@ -27,6 +27,21 @@ const GithubProvider = ({ children }) => {
     console.log(response);
     if (response) {
       setGithubUser(response.data);
+      const { login, followers_url } = response.data;
+      await Promise.allSettled([
+        axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+        axios(`${followers_url}?per_page=100`),
+      ]).then((results) => {
+        console.log(results);
+        const [repos, followers] = results;
+        const status = "fulfilled";
+        if (repos.status === status) {
+          setRepos(repos.value.data);
+        }
+        if (followers.status === status) {
+          setFollowers(followers.value.data);
+        }
+      });
     } else {
       toggleError(true, "There is no user with the given username");
     }
